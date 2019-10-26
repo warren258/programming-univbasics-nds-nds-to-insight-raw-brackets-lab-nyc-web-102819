9f83a3c3e4d096b325fa090aee2936fea2622576
Warren McDonald <warrenjmac@mail.com>
2019-10-26 01:31:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,23 +20,4 @@ def directors_totals(nds)
   #
   # Be sure to return the result at the end!
 end
-  def directors_totals(nds)
-  director_index = 0
-  results = {}
 
-  while director_index < nds.length do
-    director_name = nds[director_index][:name]
-    results[director_name] = 0
-    movie_index = 0
-
-    while movie_index < nds[director_index][:movies].length do
-      results[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
-      movie_index += 1
-    end
-
-    director_index += 1
-  end
-
-  
-end
-end
